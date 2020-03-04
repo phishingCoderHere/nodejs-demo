1b5cebe8aeeb4a6240c111027554ef14bf3e27c7
gotio <935809915@qq.com>
2020-03-04 16:41:56 +0800

    同步demo

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,69 @@
-const a = b => f2();
+// 同步方法
+async function getUsers() {
+    let stuData
+    await getStudents().then(resp => {
+        stuData = resp
+    })
+    console.log(stuData);
 
-const f2 = () => {
-
-    return {
-        fs: 'f2'
-    }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                "msg": "ok",
+                "users": [
+                    { "name": "wangwu", "age": 20 },
+                    { "name": "zhangsan", "age": 23 }
+                ]
+            })
+        }, 500);
+    })
 }
 
-console.log('a', typeof a);
-console.log('f2', typeof f2);
+// 非同步方法
+function getUsersNoAsync() {
+    let stuData
+    getStudents().then(resp => {
+        stuData = resp
+    })
+    console.log(stuData);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                "msg": "ok",
+                "users": [
+                    { "name": "wangwu", "age": 20 },
+                    { "name": "zhangsan", "age": 23 }
+                ]
+            })
+        }, 500);
+    })
+}
+
+
+function getStudents() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                "msg": "ok",
+                "users": [
+                    { "name": "zhaosi", "age": 2 },
+                    { "name": "laoli", "age": 33 }
+                ]
+            })
+        }, 1000);
+    })
+}
+
+function noSync() {
+    getUsersNoAsync().then(resp => {
+        console.log('getUser--', resp);
+    })
+}
+function sync() {
+    getUsers().then(resp => {
+        console.log('getUser--', resp);
+    })
+}
+noSync()
+sync()
--- iostom/index.js
+++ iostom/index.js
@@ -16,16 +16,15 @@ app.use('/fonts', express.static(path.join(__dirname, '/', 'pages/login/fonts'))
 app.use('/images', express.static(path.join(__dirname, '/', 'pages/login/images')))
 app.use('/public', express.static(path.join(__dirname, '/', 'pages/login/public')))
 app.use('/resources', express.static(path.join(__dirname, '/', 'pages/login/resources')))
-app.use('/', express.static(path.join(__dirname, '/', 'pages/login/resources')))
+app.use('/', express.static(path.join(__dirname, '/', '/pages/')))
 app.set('views', path.join(__dirname, '/', 'pages'))
 
 app.listen(8080, () => {
     console.log('server running on port 8080....');
-
 })
 
 app.get('/', function (req, res) {
-    res.render('welcome.html')
+    res.render('index.html')
 })
 app.get('/login', function (req, res) {
     res.render('login/login.html')
